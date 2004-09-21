321914a9fe78245f76c3a2dde4537dd3bf5afc32
Werner Koch <wk@gnupg.org>
2004-09-21 10:19:11 +0000

    (sexp_sscan): Removed c++ style commets.  Noted by Yoann Vandoorselaere.

--- src/sexp.c
+++ src/sexp.c
@@ -973,7 +973,6 @@ sexp_sscan (gcry_sexp_t *retsexp, size_t *erroff,
 		      *erroff = p - buffer;
 		      /* Invalid octal value.  */
 		      err = GPG_ERR_SEXP_BAD_QUOTATION;
-		      //return gcry_error (GPG_ERR_SEXP_BAD_QUOTATION);
 		    }
 		  p += 2;
 		  n -= 2;
@@ -986,7 +985,6 @@ sexp_sscan (gcry_sexp_t *retsexp, size_t *erroff,
 		      *erroff = p - buffer;
 		      /* Invalid hex value.  */
 		      err = GPG_ERR_SEXP_BAD_QUOTATION;
-		      //return gcry_error (GPG_ERR_SEXP_BAD_QUOTATION);
 		    }
 		  p += 2;
 		  n -= 2;
