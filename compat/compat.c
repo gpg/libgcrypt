ec033383618c4b3739783d31ca4dc70c9bb4fcfe
Werner Koch <wk@gnupg.org>
2011-03-30 11:22:56 +0200

    Add a version string to the binary

--- compat/compat.c
+++ compat/compat.c
@@ -27,7 +27,7 @@ _gcry_compat_identification (void)
 {
   static const char blurb[] =
     "\n\n"
-    "This is Libgcrypt - The GNU Crypto Library\n"
+    "This is Libgcrypt " PACKAGE_VERSION " - The GNU Crypto Library\n"
     "Copyright 2000, 2002, 2003, 2004, 2007, 2008, 2009,\n"
     "          2010, 2011 Free Software Foundation, Inc.\n"
     "\n\n";
