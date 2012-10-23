66adf76e634423bb72ce1f0b5ed78f4e4798f190
Werner Koch <wk@gnupg.org>
2012-10-23 17:52:38 +0200

    Make ancient test program useful again.
    * tests/testapi.c (test_sexp): Adjust to current API.  Print the
    return code.  Mark unused args.
    (test_genkey): Mark unused args.
    (main): Do not pass NULL to printf.

--- tests/testapi.c
+++ tests/testapi.c
@@ -44,18 +44,21 @@ test_sexp ( int argc, char **argv )
     size_t n;
     char *buf;
 
-    if ( gcry_mpi_scan( &key[0], GCRYMPI_FMT_HEX, elg_testkey1.p, NULL ) )
+    (void)argc;
+    (void)argv;
+
+    if ( gcry_mpi_scan( &key[0], GCRYMPI_FMT_HEX, elg_testkey1.p, 0, NULL ) )
 	BUG();
-    if ( gcry_mpi_scan( &key[1], GCRYMPI_FMT_HEX, elg_testkey1.g, NULL ) )
+    if ( gcry_mpi_scan( &key[1], GCRYMPI_FMT_HEX, elg_testkey1.g, 0, NULL ) )
 	BUG();
-    if ( gcry_mpi_scan( &key[2], GCRYMPI_FMT_HEX, elg_testkey1.y, NULL ) )
+    if ( gcry_mpi_scan( &key[2], GCRYMPI_FMT_HEX, elg_testkey1.y, 0, NULL ) )
 	BUG();
 
     /* get nbits from a key */
     rc = gcry_sexp_build ( &sexp, NULL,
 			   "(public-key(elg(p%m)(g%m)(y%m)))",
 				  key[0], key[1], key[2] );
-    fputs ( "DUMP of PK:\n", stderr );
+    fprintf (stderr, "DUMP of PK (rc=%d):\n", rc);
     gcry_sexp_dump ( sexp );
     {  gcry_sexp_t x;
        x = gcry_sexp_cdr ( sexp );
@@ -80,6 +83,9 @@ test_genkey ( int argc, char **argv )
     int rc, nbits = 1024;
     gcry_sexp_t s_parms, s_key;
 
+    (void)argc;
+    (void)argv;
+
     gcry_control( GCRYCTL_INIT_SECMEM, 16384, 0 );
     rc = gcry_sexp_build ( &s_parms, NULL, "(genkey(dsa(nbits %d)))", nbits );
     rc = gcry_pk_genkey( &s_key, s_parms );
@@ -95,18 +101,26 @@ test_genkey ( int argc, char **argv )
 int
 main( int argc, char **argv )
 {
-    if ( argc < 2 )
-	printf("%s\n", gcry_check_version ( NULL ) );
-    else if ( !strcmp ( argv[1], "version") )
-	printf("%s\n", gcry_check_version ( argc > 2 ? argv[2] : NULL ) );
-    else if ( !strcmp ( argv[1], "sexp" ) )
-	test_sexp ( argc-2, argv+2 );
-    else if ( !strcmp ( argv[1], "genkey" ) )
-	test_genkey ( argc-2, argv+2 );
-    else {
-	fprintf (stderr, "usage: testapi mode-string [mode-args]\n");
-	return 1;
-    }
+  const char *s;
 
-    return 0;
+  if ( argc < 2 )
+    {
+      s = gcry_check_version (NULL);
+      printf("%s\n", s? s : "(null)");
+    }
+  else if ( !strcmp ( argv[1], "version") )
+    {
+      s = gcry_check_version (argc > 2 ? argv[2] : NULL );
+      printf("%s\n", s? s : "(null)");
+    }
+  else if ( !strcmp ( argv[1], "sexp" ) )
+    test_sexp ( argc-2, argv+2 );
+  else if ( !strcmp ( argv[1], "genkey" ) )
+    test_genkey ( argc-2, argv+2 );
+  else {
+    fprintf (stderr, "usage: testapi mode-string [mode-args]\n");
+    return 1;
+  }
+
+  return 0;
 }
