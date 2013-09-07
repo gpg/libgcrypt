071f70b9a766187fc70f6abc6a69d50752449285
Werner Koch <wk@gnupg.org>
2013-09-07 10:06:46 +0200

    pk: Move RSA encoding functions to a new file.
    * cipher/rsa-common: New.
    * cipher/pubkey.c (pkcs1_encode_for_encryption): Move to rsa-common.c
    and rename to _gcry_rsa_pkcs1_encode_for_enc.
    (pkcs1_decode_for_encryption): Move to rsa-common.c and rename to
    _gcry_rsa_pkcs1_decode_for_enc.
    (pkcs1_encode_for_signature): Move to rsa-common.c and rename to
    _gcry_rsa_pkcs1_encode_for_sig.
    (oaep_encode): Move to rsa-common.c and rename to
    _gcry_rsa_oaep_encode.
    (oaep_decode): Move to rsa-common.c and rename to
    _gcry_rsa_oaep_decode.
    (pss_encode): Move to rsa-common.c and rename to _gcry_rsa_pss_encode.
    (pss_verify): Move to rsa-common.c and rename to _gcry_rsa_pss_decode.
    (octet_string_from_mpi, mgf1): Move to rsa-common.c.
    
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- cipher/pubkey-internal.h
+++ cipher/pubkey-internal.h
@@ -20,6 +20,39 @@
 #ifndef GCRY_PUBKEY_INTERNAL_H
 #define GCRY_PUBKEY_INTERNAL_H
 
+/*-- rsa-common.h --*/
+gpg_err_code_t
+_gcry_rsa_pkcs1_encode_for_enc (gcry_mpi_t *r_result, unsigned int nbits,
+                                const unsigned char *value, size_t valuelen,
+                                const unsigned char *random_override,
+                                size_t random_override_len);
+gpg_err_code_t
+_gcry_rsa_pkcs1_decode_for_enc (unsigned char **r_result, size_t *r_resultlen,
+                                unsigned int nbits, gcry_mpi_t value);
+gpg_err_code_t
+_gcry_rsa_pkcs1_encode_for_sig (gcry_mpi_t *r_result, unsigned int nbits,
+                                const unsigned char *value, size_t valuelen,
+                                int algo);
+gpg_err_code_t
+_gcry_rsa_oaep_encode (gcry_mpi_t *r_result, unsigned int nbits, int algo,
+                       const unsigned char *value, size_t valuelen,
+                       const unsigned char *label, size_t labellen,
+                       const void *random_override, size_t random_override_len);
+gpg_err_code_t
+_gcry_rsa_oaep_decode (unsigned char **r_result, size_t *r_resultlen,
+                       unsigned int nbits, int algo,
+                       gcry_mpi_t value,
+                       const unsigned char *label, size_t labellen);
+gpg_err_code_t
+_gcry_rsa_pss_encode (gcry_mpi_t *r_result, unsigned int nbits, int algo,
+                      const unsigned char *value, size_t valuelen, int saltlen,
+                      const void *random_override, size_t random_override_len);
+gpg_err_code_t
+_gcry_rsa_pss_verify (gcry_mpi_t value, gcry_mpi_t encoded,
+                      unsigned int nbits, int algo, size_t saltlen);
+
+
+
 /*-- dsa-common.h --*/
 gcry_mpi_t _gcry_dsa_gen_k (gcry_mpi_t q, int security_level);
 gpg_err_code_t _gcry_dsa_gen_rfc6979_k (gcry_mpi_t *r_k,
