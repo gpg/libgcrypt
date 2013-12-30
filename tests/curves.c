9bedc5c3b646dfe481678ca58f5466ac46decaf7
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2013-12-31 00:39:57 +0400

    Add GOST R 34.10-2001 curves per RFC4357
    * cipher/ecc-curves.c (domain_parms): Add 3 curves defined in rfc4357.
    * cipher/ecc-curves.c (curve_aliases): Add OID and Xch aliases for GOST
      curves.
    * tests/curves.c (N_CURVES): Update value.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- tests/curves.c
+++ tests/curves.c
@@ -29,7 +29,7 @@
 #include "../src/gcrypt-int.h"
 
 /* Number of curves defined in ../cipger/ecc.c */
-#define N_CURVES 16
+#define N_CURVES 19
 
 /* A real world sample public key.  */
 static char const sample_key_1[] =
