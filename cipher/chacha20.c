a3062db748f272e0f7346e1ed9e0bf7ed61a4eae
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2014-05-12 20:11:33 +0300

    chacha20/AVX2: check for ENABLE_AVX2_SUPPORT instead of HAVE_GCC_INLINE_ASM_AVX2
    * cipher/chacha20.c (USE_AVX2): Enable depending on
    ENABLE_AVX2_SUPPORT, not HAVE_GCC_INLINE_ASM_AVX2.
    * cipher/chacha20-avx2-amd64.S: Ditto.
    --
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- cipher/chacha20.c
+++ cipher/chacha20.c
@@ -57,7 +57,7 @@
 /* USE_AVX2 indicates whether to compile with Intel AVX2 code. */
 #undef USE_AVX2
 #if defined(__x86_64__) && defined(HAVE_COMPATIBLE_GCC_AMD64_PLATFORM_AS) && \
-    defined(HAVE_GCC_INLINE_ASM_AVX2)
+    defined(ENABLE_AVX2_SUPPORT)
 # define USE_AVX2 1
 #endif
 
