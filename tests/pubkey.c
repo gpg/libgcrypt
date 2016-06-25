c870cb5d385c1d6e1e28ca481cf9cf44b3bfeea9
Werner Koch <wk@gnupg.org>
2016-06-25 16:09:20 +0200

    tests: Add attributes to helper functions.
    * tests/t-common.h (die, fail, info): Add attributes.
    * tests/random.c (die, inf): Ditto.
    * tests/pubkey.c (die, fail, info): Add attributes.
    * tests/fipsdrv.c (die): Add attribute.
    (main): Take care of missing --key,--iv,--dt options.
    
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- tests/pubkey.c
+++ tests/pubkey.c
@@ -115,6 +115,15 @@ static const char sample_public_key_1[] =
 static int verbose;
 static int error_count;
 
+
+/* If we have a decent libgpg-error we can use some gcc attributes.  */
+#ifdef GPGRT_ATTR_NORETURN
+static void die (const char *format, ...) GPGRT_ATTR_NR_PRINTF(1,2);
+static void fail (const char *format, ...) GPGRT_ATTR_PRINTF(1,2);
+static void info (const char *format, ...) GPGRT_ATTR_PRINTF(1,2);
+#endif /*GPGRT_ATTR_NORETURN*/
+
+
 static void
 die (const char *format, ...)
 {
