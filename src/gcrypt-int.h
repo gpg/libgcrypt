cf1528e7f2761774d06ace0de48f39c96b52dc4f
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2017-07-29 14:34:23 +0300

    Fix return value type for _gcry_md_extract
    * src/gcrypt-int.h (_gcry_md_extract): Use gpg_err_code_t instead of
    gpg_error_t for internal function return type.
    --
    
    GnuPG-bug-id: 3314
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- src/gcrypt-int.h
+++ src/gcrypt-int.h
@@ -39,7 +39,7 @@ typedef struct mpi_ec_ctx_s *mpi_ec_t;
 
 
 /* Underscore prefixed internal versions of the public functions.
-   They return gpg_err_code and not gpg_error_t.  Some macros also
+   They return gpg_err_code_t and not gpg_error_t.  Some macros also
    need an underscore prefixed internal version.
 
    Note that the memory allocation functions and macros (xmalloc etc.)
@@ -120,8 +120,8 @@ gpg_err_code_t _gcry_md_ctl (gcry_md_hd_t hd, int cmd,
                           void *buffer, size_t buflen);
 void _gcry_md_write (gcry_md_hd_t hd, const void *buffer, size_t length);
 unsigned char *_gcry_md_read (gcry_md_hd_t hd, int algo);
-gpg_error_t _gcry_md_extract (gcry_md_hd_t hd, int algo, void *buffer,
-                              size_t length);
+gpg_err_code_t _gcry_md_extract (gcry_md_hd_t hd, int algo, void *buffer,
+                                 size_t length);
 void _gcry_md_hash_buffer (int algo, void *digest,
                            const void *buffer, size_t length);
 gpg_err_code_t _gcry_md_hash_buffers (int algo, unsigned int flags,
