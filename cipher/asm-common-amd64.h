d11ae95d05dc39ec6b825d1109afadd964589880
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2019-04-15 19:46:53 +0300

    Add CFI unwind assembly directives for AMD64 assembly
    * configure.ac (gcry_cv_gcc_asm_cfi_directives): New.
    * cipher/asm-common-amd64.h (ADD_RIP, CFI_STARTPROC, CFI_ENDPROC)
    (CFI_REMEMBER_STATE, CFI_RESTORE_STATE, CFI_ADJUST_CFA_OFFSET)
    (CFI_REL_OFFSET, CFI_DEF_CFA_REGISTER, CFI_REGISTER, CFI_RESTORE)
    (CFI_PUSH, CFI_POP, CFI_POP_TMP_REG, CFI_LEAVE, DW_REGNO)
    (DW_SLEB128_7BIT, DW_SLEB128_28BIT, CFI_CFA_ON_STACK)
    (CFI_REG_ON_STACK): New.
    (ENTER_SYSV_FUNCPARAMS_0_4, EXIT_SYSV_FUNC): Add CFI directives.
    * cipher/arcfour-amd64.S: Add CFI directives.
    * cipher/blake2b-amd64-avx2.S: Add CFI directives.
    * cipher/blake2s-amd64-avx.S: Add CFI directives.
    * cipher/blowfish-amd64.S: Add CFI directives.
    * cipher/camellia-aesni-avx-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/camellia-aesni-avx2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/cast5-amd64.S: Add CFI directives.
    * cipher/chacha20-amd64-avx2.S: Add CFI directives.
    * cipher/chacha20-amd64-ssse3.S: Add CFI directives.
    * cipher/des-amd64.S: Add CFI directives.
    * cipher/rijndael-amd64.S: Add CFI directives.
    * cipher/rijndael-ssse3-amd64-asm.S: Add CFI directives.
    * cipher/salsa20-amd64.S: Add CFI directives; Use 'asm-common-amd64.h'.
    * cipher/serpent-avx2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/serpent-sse2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha1-avx-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha1-avx-bmi2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha1-avx2-bmi2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha1-ssse3-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha256-avx-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha256-avx2-bmi2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha256-ssse3-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha512-avx-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha512-avx2-bmi2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/sha512-ssse3-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/twofish-amd64.S: Add CFI directives.
    * cipher/twofish-avx2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * cipher/whirlpool-sse2-amd64.S: Add CFI directives; Use
    'asm-common-amd64.h'.
    * mpi/amd64/func_abi.h: Include 'config.h'.
    (CFI_STARTPROC, CFI_ENDPROC, CFI_ADJUST_CFA_OFFSET, CFI_REL_OFFSET)
    (CFI_RESTORE, CFI_PUSH, CFI_POP): New.
    (FUNC_ENTRY, FUNC_EXIT): Add CFI directives.
    --
    
    This commit adds CFI directives that add DWARF unwinding information for
    debugger to backtrace when executing code from AMD64 assembly files.
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- cipher/asm-common-amd64.h
+++ cipher/asm-common-amd64.h
@@ -41,6 +41,12 @@
 #  define RIP
 #endif
 
+#ifdef __PIC__
+#  define ADD_RIP +rip
+#else
+#  define ADD_RIP
+#endif
+
 #if defined(HAVE_COMPATIBLE_GCC_WIN64_PLATFORM_AS) || !defined(__PIC__)
 #  define GET_EXTERN_POINTER(name, reg) movabsq $name, reg
 #else
@@ -60,10 +66,101 @@
 #  endif
 #endif
 
+#ifdef HAVE_GCC_ASM_CFI_DIRECTIVES
+/* CFI directives to emit DWARF stack unwinding information. */
+# define CFI_STARTPROC()            .cfi_startproc
+# define CFI_ENDPROC()              .cfi_endproc
+# define CFI_REMEMBER_STATE()       .cfi_remember_state
+# define CFI_RESTORE_STATE()        .cfi_restore_state
+# define CFI_ADJUST_CFA_OFFSET(off) .cfi_adjust_cfa_offset off
+# define CFI_REL_OFFSET(reg,off)    .cfi_rel_offset reg, off
+# define CFI_DEF_CFA_REGISTER(reg)  .cfi_def_cfa_register reg
+# define CFI_REGISTER(ro,rn)        .cfi_register ro, rn
+# define CFI_RESTORE(reg)           .cfi_restore reg
+
+# define CFI_PUSH(reg) \
+	CFI_ADJUST_CFA_OFFSET(8); CFI_REL_OFFSET(reg, 0)
+# define CFI_POP(reg) \
+	CFI_ADJUST_CFA_OFFSET(-8); CFI_RESTORE(reg)
+# define CFI_POP_TMP_REG() \
+	CFI_ADJUST_CFA_OFFSET(-8);
+# define CFI_LEAVE() \
+	CFI_ADJUST_CFA_OFFSET(-8); CFI_DEF_CFA_REGISTER(%rsp)
+
+/* CFA expressions are used for pointing CFA and registers to
+ * %rsp relative offsets. */
+# define DW_REGNO_rax 0
+# define DW_REGNO_rdx 1
+# define DW_REGNO_rcx 2
+# define DW_REGNO_rbx 3
+# define DW_REGNO_rsi 4
+# define DW_REGNO_rdi 5
+# define DW_REGNO_rbp 6
+# define DW_REGNO_rsp 7
+# define DW_REGNO_r8  8
+# define DW_REGNO_r9  9
+# define DW_REGNO_r10 10
+# define DW_REGNO_r11 11
+# define DW_REGNO_r12 12
+# define DW_REGNO_r13 13
+# define DW_REGNO_r14 14
+# define DW_REGNO_r15 15
+
+# define DW_REGNO(reg) DW_REGNO_ ## reg
+
+/* Fixed length encoding used for integers for now. */
+# define DW_SLEB128_7BIT(value) \
+	0x00|((value) & 0x7f)
+# define DW_SLEB128_28BIT(value) \
+	0x80|((value)&0x7f), \
+	0x80|(((value)>>7)&0x7f), \
+	0x80|(((value)>>14)&0x7f), \
+	0x00|(((value)>>21)&0x7f)
+
+# define CFI_CFA_ON_STACK(rsp_offs,cfa_depth) \
+	.cfi_escape \
+	  0x0f, /* DW_CFA_def_cfa_expression */ \
+	    DW_SLEB128_7BIT(11), /* length */ \
+	  0x77, /* DW_OP_breg7, rsp + constant */ \
+	    DW_SLEB128_28BIT(rsp_offs), \
+	  0x06, /* DW_OP_deref */ \
+	  0x23, /* DW_OP_plus_constu */ \
+	    DW_SLEB128_28BIT((cfa_depth)+8)
+
+# define CFI_REG_ON_STACK(reg,rsp_offs) \
+	.cfi_escape \
+	  0x10, /* DW_CFA_expression */ \
+	    DW_SLEB128_7BIT(DW_REGNO(reg)), \
+	    DW_SLEB128_7BIT(5), /* length */ \
+	  0x77, /* DW_OP_breg7, rsp + constant */ \
+	    DW_SLEB128_28BIT(rsp_offs)
+
+#else
+# define CFI_STARTPROC()
+# define CFI_ENDPROC()
+# define CFI_REMEMBER_STATE()
+# define CFI_RESTORE_STATE()
+# define CFI_ADJUST_CFA_OFFSET(off)
+# define CFI_REL_OFFSET(reg,off)
+# define CFI_DEF_CFA_REGISTER(reg)
+# define CFI_REGISTER(ro,rn)
+# define CFI_RESTORE(reg)
+
+# define CFI_PUSH(reg)
+# define CFI_POP(reg)
+# define CFI_POP_TMP_REG()
+# define CFI_LEAVE()
+
+# define CFI_CFA_ON_STACK(rsp_offs,cfa_depth)
+# define CFI_REG_ON_STACK(reg,rsp_offs)
+#endif
+
 #ifdef HAVE_COMPATIBLE_GCC_WIN64_PLATFORM_AS
 # define ENTER_SYSV_FUNC_PARAMS_0_4 \
 	pushq %rdi; \
+	CFI_PUSH(%rdi); \
 	pushq %rsi; \
+	CFI_PUSH(%rsi); \
 	movq %rcx, %rdi; \
 	movq %rdx, %rsi; \
 	movq %r8, %rdx; \
@@ -79,7 +176,9 @@
 
 # define EXIT_SYSV_FUNC \
 	popq %rsi; \
-	popq %rdi;
+	CFI_POP(%rsi); \
+	popq %rdi; \
+	CFI_POP(%rdi);
 #else
 # define ENTER_SYSV_FUNC_PARAMS_0_4
 # define ENTER_SYSV_FUNC_PARAMS_5
--- mpi/amd64/func_abi.h
+++ mpi/amd64/func_abi.h
@@ -1,9 +1,36 @@
+#include <config.h>
+
+#ifdef HAVE_GCC_ASM_CFI_DIRECTIVES
+# define CFI_STARTPROC()            .cfi_startproc
+# define CFI_ENDPROC()              .cfi_endproc
+# define CFI_ADJUST_CFA_OFFSET(off) .cfi_adjust_cfa_offset off
+# define CFI_REL_OFFSET(reg,off)    .cfi_rel_offset reg, off
+# define CFI_RESTORE(reg)           .cfi_restore reg
+
+# define CFI_PUSH(reg) \
+	CFI_ADJUST_CFA_OFFSET(8); CFI_REL_OFFSET(reg, 0)
+# define CFI_POP(reg) \
+	CFI_ADJUST_CFA_OFFSET(-8); CFI_RESTORE(reg)
+#else
+# define CFI_STARTPROC()
+# define CFI_ENDPROC()
+# define CFI_ADJUST_CFA_OFFSET(off)
+# define CFI_REL_OFFSET(reg,off)
+# define CFI_RESTORE(reg)
+
+# define CFI_PUSH(reg)
+# define CFI_POP(reg)
+#endif
+
 #ifdef USE_MS_ABI
  /* Store registers and move four first input arguments from MS ABI to
   * SYSV ABI.  */
  #define FUNC_ENTRY() \
+	CFI_STARTPROC(); \
 	pushq %rsi; \
+	CFI_PUSH(%rsi); \
 	pushq %rdi; \
+	CFI_PUSH(%rdi); \
 	movq %rdx, %rsi; \
 	movq %rcx, %rdi; \
 	movq %r8, %rdx; \
@@ -12,8 +39,16 @@
  /* Restore registers.  */
  #define FUNC_EXIT() \
 	popq %rdi; \
-	popq %rsi;
+	CFI_POP(%rdi); \
+	popq %rsi; \
+	CFI_POP(%rsi); \
+	ret; \
+	CFI_ENDPROC();
 #else
- #define FUNC_ENTRY() /**/
- #define FUNC_EXIT() /**/
+ #define FUNC_ENTRY() \
+	CFI_STARTPROC();
+
+ #define FUNC_EXIT() \
+	ret; \
+	CFI_ENDPROC();
 #endif
