1862f402d363dce946c3169d4f4f48c5eee052f1
NIIBE Yutaka <gniibe@fsij.org>
2019-08-09 11:53:53 +0900

    ecdsa: Fix unblinding too early.
    * cipher/ecc-ecdsa.c (_gcry_ecc_ecdsa_sign): Keep the blinding until
    the last step.
    
    --
    
    Cherry-picked master commit of:
    	cdaeb86f067b94d9dff4235ade20dde6479d9bb8
    
    Suggested-by: David Schrammel <david.schrammel@iaik.tugraz.at>
    Suggested-by: Samuel Weiser <samuel.weiser@iaik.tugraz.at>
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- cipher/ecc-ecdsa.c
+++ cipher/ecc-ecdsa.c
@@ -128,13 +128,15 @@ _gcry_ecc_ecdsa_sign (gcry_mpi_t input, ECC_secret_key *skey,
         }
       while (!mpi_cmp_ui (r, 0));
 
+      /* Computation of dr, sum, and s are blinded with b.  */
       mpi_mulm (dr, b, skey->d, skey->E.n);
-      mpi_mulm (dr, dr, r, skey->E.n);      /* dr = d*r mod n (blinded with b) */
+      mpi_mulm (dr, dr, r, skey->E.n);      /* dr = d*r mod n */
       mpi_mulm (sum, b, hash, skey->E.n);
-      mpi_addm (sum, sum, dr, skey->E.n);   /* sum = hash + (d*r) mod n  (blinded with b) */
-      mpi_mulm (sum, bi, sum, skey->E.n);   /* undo blinding by b^-1 */
+      mpi_addm (sum, sum, dr, skey->E.n);   /* sum = hash + (d*r) mod n */
       mpi_invm (k_1, k, skey->E.n);         /* k_1 = k^(-1) mod n  */
       mpi_mulm (s, k_1, sum, skey->E.n);    /* s = k^(-1)*(hash+(d*r)) mod n */
+      /* Undo blinding by b^-1 */
+      mpi_mulm (s, bi, s, skey->E.n);
     }
   while (!mpi_cmp_ui (s, 0));
 
