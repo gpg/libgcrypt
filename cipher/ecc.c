95cc9b8f4483fd7edfc7555199f6a05cfa68a236
NIIBE Yutaka <gniibe@fsij.org>
2019-10-18 09:44:01 +0900

    ecc: Clean up key generation code.
    * cipher/ecc.c (test_ecdh_only_keys): No need to make PK by SK.
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- cipher/ecc.c
+++ cipher/ecc.c
@@ -327,7 +327,6 @@ test_keys (ECC_secret_key *sk, unsigned int nbits)
 static void
 test_ecdh_only_keys (ECC_secret_key *sk, unsigned int nbits, int flags)
 {
-  ECC_public_key pk;
   gcry_mpi_t test;
   mpi_point_struct R_;
   gcry_mpi_t x0, x1;
@@ -337,10 +336,6 @@ test_ecdh_only_keys (ECC_secret_key *sk, unsigned int nbits, int flags)
     log_debug ("Testing ECDH only key.\n");
 
   point_init (&R_);
-
-  pk.E = _gcry_ecc_curve_copy (sk->E);
-  point_init (&pk.Q);
-  point_set (&pk.Q, &sk->Q);
 
   if ((flags & PUBKEY_FLAG_DJB_TWEAK))
     {
@@ -365,19 +360,19 @@ test_ecdh_only_keys (ECC_secret_key *sk, unsigned int nbits, int flags)
       _gcry_mpi_randomize (test, nbits, GCRY_WEAK_RANDOM);
     }
 
-  ec = _gcry_mpi_ec_p_internal_new (pk.E.model, pk.E.dialect, flags,
-                                    pk.E.p, pk.E.a, pk.E.b);
+  ec = _gcry_mpi_ec_p_internal_new (sk->E.model, sk->E.dialect, flags,
+                                    sk->E.p, sk->E.a, sk->E.b);
   x0 = mpi_new (0);
   x1 = mpi_new (0);
 
   /* R_ = hkQ  <=>  R_ = hkdG  */
-  _gcry_mpi_ec_mul_point (&R_, test, &pk.Q, ec);
+  _gcry_mpi_ec_mul_point (&R_, test, &sk->Q, ec);
   if (!(flags & PUBKEY_FLAG_DJB_TWEAK))
     _gcry_mpi_ec_mul_point (&R_, ec->h, &R_, ec);
   if (_gcry_mpi_ec_get_affine (x0, NULL, &R_, ec))
     log_fatal ("ecdh: Failed to get affine coordinates for hkQ\n");
 
-  _gcry_mpi_ec_mul_point (&R_, test, &pk.E.G, ec);
+  _gcry_mpi_ec_mul_point (&R_, test, &sk->E.G, ec);
   _gcry_mpi_ec_mul_point (&R_, sk->d, &R_, ec);
   /* R_ = hdkG */
   if (!(flags & PUBKEY_FLAG_DJB_TWEAK))
@@ -394,9 +389,6 @@ test_ecdh_only_keys (ECC_secret_key *sk, unsigned int nbits, int flags)
   mpi_free (x0);
   mpi_free (x1);
   _gcry_mpi_ec_free (ec);
-
-  point_free (&pk.Q);
-  _gcry_ecc_curve_free (&pk.E);
 
   point_free (&R_);
   mpi_free (test);
