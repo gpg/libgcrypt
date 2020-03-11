b4b04ae6c2e55bc2b24efc663d1eeaa0b3613f4c
NIIBE Yutaka <gniibe@fsij.org>
2020-03-11 16:09:07 +0900

    mpi: Support opaque MPI with gcry_mpi_print.
    * mpi/mpicoder.c (_gcry_mpi_get_buffer): Return the bytes as-is.
    
    GnuPG-bug-id: 4872
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- mpi/mpicoder.c
+++ mpi/mpicoder.c
@@ -298,7 +298,18 @@ byte *
 _gcry_mpi_get_buffer (gcry_mpi_t a, unsigned int fill_le,
                       unsigned int *r_nbytes, int *sign)
 {
-  return do_get_buffer (a, fill_le, 0, r_nbytes, sign, 0);
+  if (mpi_get_flag (a, GCRYMPI_FLAG_OPAQUE))
+    {
+      unsigned int nbits;
+      byte *p = _gcry_mpi_get_opaque_copy (a, &nbits);
+
+      if (r_nbytes)
+        *r_nbytes = (nbits+7)/8;
+
+      return p;
+    }
+  else
+    return do_get_buffer (a, fill_le, 0, r_nbytes, sign, 0);
 }
 
 byte *
