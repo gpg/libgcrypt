836e52a942c4f846f76145ddafc554f4564ea04b
NIIBE Yutaka <gniibe@fsij.org>
2021-09-02 15:50:50 +0900

    experiment: New PKEY API.
    * src/gcrypt-int.h: Add functions.
    * src/gcrypt.h.in: Add enum and typedef.
    
    --
    
    Commits here are not expected to merge directly.  Instead,
    please review again and manually pick to merge.
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- src/gcrypt-int.h
+++ src/gcrypt-int.h
@@ -113,7 +113,14 @@ unsigned int _gcry_ecc_get_algo_keylen (int algo);
 gpg_error_t _gcry_ecc_mul_point (int algo, unsigned char *result,
                                  const unsigned char *scalar,
                                  const unsigned char *point);
-
+
+gcry_error_t _gcry_pkey_open (gcry_pkey_hd_t *h, int algo, ...);
+gcry_error_t _gcry_pkey_ctl (gcry_pkey_hd_t h, int cmd,
+                             void *buffer, size_t buflen);
+gcry_error_t _gcry_pkey_op (gcry_pkey_hd_t h, int cmd,
+                            int num_in, const unsigned char *in[], size_t in_len[],
+                            int num_out, unsigned char *out[], size_t *out_len[]);
+gcry_error_t _gcry_pkey_close (gcry_pkey_hd_t h);
 
 gpg_err_code_t _gcry_md_open (gcry_md_hd_t *h, int algo, unsigned int flags);
 void _gcry_md_close (gcry_md_hd_t hd);
