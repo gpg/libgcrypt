301df435e0ac5a74d93d388df9a5c9e20bb430ce
NIIBE Yutaka <gniibe@fsij.org>
2021-09-22 11:19:49 +0900

    experiment: Test gcry_pk_hash_sign/gcry_pk_hash_verify for DSA.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- tests/t-dsa.c
+++ tests/t-dsa.c
@@ -357,6 +357,283 @@ one_test (const char *sha_alg, const char *p, const char *q, const char *g,
 
 
 static void
+one_test_sexp (const char *sha_alg,
+               const char *p, const char *q, const char *g,
+               const char *x, const char *y,
+               const char *msg, const char *k,
+               const char *r, const char *s)
+{
+  gpg_error_t err;
+  int i;
+  char *p0;
+  void *buffer = NULL;
+  void *buffer2 = NULL;
+  void *buffer3 = NULL;
+  void *buffer4 = NULL;
+  void *buffer5 = NULL;
+  size_t buflen, buflen2, buflen3, buflen4, buflen5;
+  gcry_ctx_t ctx = NULL;
+  int md_algo;
+  const char *data_tmpl;
+  gcry_md_hd_t hd = NULL;
+  gcry_sexp_t s_pk = NULL;
+  gcry_sexp_t s_sk = NULL;
+  gcry_sexp_t s_sig= NULL;
+  gcry_sexp_t s_tmp, s_tmp2;
+  unsigned char *out_r = NULL;
+  unsigned char *out_s = NULL;
+  size_t out_r_len, out_s_len;
+  char *sig_r_string = NULL;
+  char *sig_s_string = NULL;
+
+  if (verbose > 1)
+    info ("Running test %s\n", sha_alg);
+
+  if (!strcmp (sha_alg, "SHA-1"))
+    md_algo = GCRY_MD_SHA1;
+  else if (!strcmp (sha_alg, "SHA-224"))
+    md_algo = GCRY_MD_SHA224;
+  else if (!strcmp (sha_alg, "SHA-256"))
+    md_algo = GCRY_MD_SHA256;
+  else if (!strcmp (sha_alg, "SHA-384"))
+    md_algo = GCRY_MD_SHA384;
+  else if (!strcmp (sha_alg, "SHA-512"))
+    md_algo = GCRY_MD_SHA512;
+  else
+    {
+      fail ("error for test, %s: %s: %s",
+            "SHA algo", "invalid", sha_alg);
+      goto leave;
+    }
+
+  err = gcry_md_open (&hd, md_algo, 0);
+  if (err)
+    {
+      fail ("algo %d, gcry_md_open failed: %s\n", md_algo, gpg_strerror (err));
+      return;
+    }
+
+  if (!(buffer = hex2buffer (p, &buflen)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "p", "invalid hex string");
+      goto leave;
+    }
+  if (!(buffer2 = hex2buffer (q, &buflen2)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "q", "invalid hex string");
+      goto leave;
+    }
+  if (!(buffer3 = hex2buffer (g, &buflen3)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "g", "invalid hex string");
+      goto leave;
+    }
+  if (!(buffer4 = hex2buffer (y, &buflen4)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "y", "invalid hex string");
+      goto leave;
+    }
+  if (!(buffer5 = hex2buffer (x, &buflen5)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "x", "invalid hex string");
+      goto leave;
+    }
+
+  err = gcry_sexp_build (&s_sk, NULL,
+                         "(private-key (dsa (p %b)(q %b)(g %b)(y %b)(x %b)))",
+                         (int)buflen, buffer,
+                         (int)buflen2, buffer2,
+                         (int)buflen3, buffer3,
+                         (int)buflen4, buffer4,
+                         (int)buflen5, buffer5);
+  if (err)
+    {
+      fail ("error building SEXP for test, %s: %s",
+            "sk", gpg_strerror (err));
+      goto leave;
+    }
+
+  err = gcry_sexp_build (&s_pk, NULL,
+                         "(public-key (dsa (p %b)(q %b)(g %b)(y %b)))",
+                         (int)buflen, buffer,
+                         (int)buflen2, buffer2,
+                         (int)buflen3, buffer3,
+                         (int)buflen4, buffer4);
+  if (err)
+    {
+      fail ("error building SEXP for test, %s: %s",
+            "pk", gpg_strerror (err));
+      goto leave;
+    }
+
+  xfree (buffer);
+  xfree (buffer2);
+  xfree (buffer3);
+  xfree (buffer4);
+  xfree (buffer5);
+  buffer = buffer2 = buffer3 = buffer4 = buffer5 = NULL;
+
+  if (!(buffer = hex2buffer (msg, &buflen)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "msg", "invalid hex string");
+      goto leave;
+    }
+
+  gcry_md_write (hd, buffer, buflen);
+  xfree (buffer);
+  buffer = NULL;
+
+  if (!(buffer2 = hex2buffer (k, &buflen2)))
+    {
+      fail ("error parsing for test, %s: %s",
+            "salt_val", "invalid hex string");
+      goto leave;
+    }
+
+  err = gcry_pk_random_override_new (&ctx, buffer2, buflen2);
+  if (err)
+    {
+      fail ("error setting salt for test: %s",
+	    gpg_strerror (err));
+      goto leave;
+    }
+
+  xfree (buffer2);
+  buffer2 = NULL;
+
+  data_tmpl = "(data(flags raw)(hash %s %b)(label %b))";
+
+  err = gcry_pk_hash_sign (&s_sig, data_tmpl, s_sk, hd, ctx);
+  if (err)
+    {
+      fail ("gcry_pkey_hash_sign failed: %s", gpg_strerror (err));
+      goto leave;
+    }
+
+  out_r_len = out_s_len = 0;
+  out_s = out_r = NULL;
+  s_tmp2 = NULL;
+  s_tmp = gcry_sexp_find_token (s_sig, "sig-val", 0);
+  if (s_tmp)
+    {
+      s_tmp2 = s_tmp;
+      s_tmp = gcry_sexp_find_token (s_tmp2, "dsa", 0);
+      if (s_tmp)
+        {
+          gcry_sexp_release (s_tmp2);
+          s_tmp2 = s_tmp;
+          s_tmp = gcry_sexp_find_token (s_tmp2, "r", 0);
+          if (s_tmp)
+            {
+              const char *p1;
+              size_t n;
+
+              out_r_len = buflen2;
+              out_r = xmalloc (out_r_len);
+              if (!out_r)
+                {
+                  err = gpg_error_from_syserror ();
+                  gcry_sexp_release (s_tmp);
+                  gcry_sexp_release (s_tmp2);
+		  goto leave;
+                }
+
+              p1 = gcry_sexp_nth_data (s_tmp, 1, &n);
+              if (n == out_r_len)
+                memcpy (out_r, p1, out_r_len);
+              else
+                {
+                  memset (out_r, 0, out_r_len - n);
+                  memcpy (out_r + out_r_len - n, p1, n);
+                }
+              gcry_sexp_release (s_tmp);
+            }
+          s_tmp = gcry_sexp_find_token (s_tmp2, "s", 0);
+          if (s_tmp)
+            {
+              const char *p1;
+              size_t n;
+
+              out_s_len = out_r_len;
+              out_s = xmalloc (out_s_len);
+              if (!out_s)
+                {
+                  err = gpg_error_from_syserror ();
+                  gcry_sexp_release (s_tmp);
+                  gcry_sexp_release (s_tmp2);
+		  goto leave;
+                }
+
+              p1 = gcry_sexp_nth_data (s_tmp, 1, &n);
+              if (n == out_s_len)
+                memcpy (out_s, p1, out_s_len);
+              else
+                {
+                  memset (out_s, 0, out_s_len - n);
+                  memcpy (out_s + out_s_len - n, p1, n);
+                }
+              gcry_sexp_release (s_tmp);
+            }
+        }
+    }
+  gcry_sexp_release (s_tmp2);
+
+  sig_r_string = xmalloc (2*out_r_len+1);
+  p0 = sig_r_string;
+  *p0 = 0;
+  for (i=0; i < out_r_len; i++, p0 += 2)
+    snprintf (p0, 3, "%02x", out_r[i]);
+
+  sig_s_string = xmalloc (2*out_s_len+1);
+  p0 = sig_s_string;
+  *p0 = 0;
+  for (i=0; i < out_s_len; i++, p0 += 2)
+    snprintf (p0, 3, "%02x", out_s[i]);
+
+  if (strcmp (sig_r_string, r) || strcmp (sig_s_string, s))
+    {
+      fail ("gcry_pk_hash_sign failed: %s",
+            "wrong value returned");
+      info ("  expected: '%s'", r);
+      info ("       got: '%s'", sig_r_string);
+      info ("  expected: '%s'", s);
+      info ("       got: '%s'", sig_s_string);
+    }
+
+  if (!no_verify)
+    {
+      err = gcry_pk_hash_verify (s_sig, data_tmpl, s_pk, hd, ctx);
+      if (err)
+        fail ("gcry_pk_hash_verify failed for test: %s",
+              gpg_strerror (err));
+    }
+
+ leave:
+  gcry_ctx_release (ctx);
+  gcry_sexp_release (s_sig);
+  gcry_sexp_release (s_sk);
+  gcry_sexp_release (s_pk);
+  if (hd)
+    gcry_md_close (hd);
+  xfree (buffer);
+  xfree (buffer2);
+  xfree (buffer3);
+  xfree (buffer4);
+  xfree (buffer5);
+  xfree (out_r);
+  xfree (out_s);
+  xfree (sig_r_string);
+  xfree (sig_s_string);
+}
+
+
+static void
 check_dsa (const char *fname)
 {
   FILE *fp;
@@ -405,6 +682,7 @@ check_dsa (const char *fname)
       if (sha_alg && p && q && g && msg && x && y && k && r && s)
         {
           one_test (sha_alg, p, q, g, x, y, msg, k, r, s);
+          one_test_sexp (sha_alg, p, q, g, x, y, msg, k, r, s);
           ntests++;
           if (!(ntests % 256))
             show_note ("%d of %d tests done\n", ntests, N_TESTS);
