ce60a68a1172ea20c1ff72e27e4b0115d805bf48
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2022-09-28 20:29:25 +0300

    cipher-ccm: remove VLA usage
    * cipher/cipher-ccm.c (do_cbc_mac): Avoid VLA for stack array.
    --
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- cipher/cipher-ccm.c
+++ cipher/cipher-ccm.c
@@ -38,9 +38,9 @@ static unsigned int
 do_cbc_mac (gcry_cipher_hd_t c, const unsigned char *inbuf, size_t inlen,
             int do_padding)
 {
-  const unsigned int blocksize = 16;
   gcry_cipher_encrypt_t enc_fn = c->spec->encrypt;
-  unsigned char tmp[blocksize];
+  unsigned char tmp[16];
+  const unsigned int blocksize = DIM(tmp);
   unsigned int burn = 0;
   unsigned int unused = c->u_mode.ccm.mac_unused;
   size_t nblocks;
