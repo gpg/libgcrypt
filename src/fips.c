05cb8355d3e66f15425ad85ae2203882e80f4792
Jakub Jelen <jjelen@redhat.com>
2022-10-05 16:50:08 +0200

    fips: Mark gcry_pk_encrypt/decrypt function non-approved.
    * src/fips.c (_gcry_fips_indicator_function): Add
    gcry_pk_encrypt/decrypt as non-approved.
    
    --
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/fips.c
+++ src/fips.c
@@ -396,7 +396,9 @@ _gcry_fips_indicator_function (va_list arg_ptr)
   const char *function = va_arg (arg_ptr, const char *);
 
   if (strcmp (function, "gcry_pk_sign") == 0 ||
-      strcmp (function, "gcry_pk_verify") == 0)
+      strcmp (function, "gcry_pk_verify") == 0 ||
+      strcmp (function, "gcry_pk_encrypt") == 0 ||
+      strcmp (function, "gcry_pk_decrypt") == 0)
     return GPG_ERR_NOT_SUPPORTED;
 
   return GPG_ERR_NO_ERROR;
