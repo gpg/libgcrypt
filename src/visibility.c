fc19b27b543910833096a738dae0703b3dc57d51
Jakub Jelen <jjelen@redhat.com>
2023-03-01 17:14:00 +0100

    visibility: Check FIPS operational status for MD+Sign operation.
    * src/visibility.c (gcry_pk_hash_sign): Check fips status before
    calling the operation itself.
    (gcry_pk_hash_verify): Ditto.
    
    --
    
    Cherry-picked master commit of:
    	654d0dfa04993ebe28c0536d42f4bc6d87c28369
    
    GnuPG-bug-id: 6396
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/visibility.c
+++ src/visibility.c
@@ -1034,6 +1034,11 @@ gcry_error_t
 gcry_pk_hash_sign (gcry_sexp_t *result, const char *data_tmpl, gcry_sexp_t skey,
                    gcry_md_hd_t hd, gcry_ctx_t ctx)
 {
+  if (!fips_is_operational ())
+    {
+      *result = NULL;
+      return gpg_error (fips_not_operational ());
+    }
   return gpg_error (_gcry_pk_sign_md (result, data_tmpl, hd, skey, ctx));
 }
 
@@ -1049,6 +1054,8 @@ gcry_error_t
 gcry_pk_hash_verify (gcry_sexp_t sigval, const char *data_tmpl, gcry_sexp_t pkey,
                      gcry_md_hd_t hd, gcry_ctx_t ctx)
 {
+  if (!fips_is_operational ())
+    return gpg_error (fips_not_operational ());
   return gpg_error (_gcry_pk_verify_md (sigval, data_tmpl, hd, pkey, ctx));
 }
 
