34c20427926010d6fa95b1666e4b1b60f60a8742
NIIBE Yutaka <gniibe@fsij.org>
2023-10-27 14:03:40 +0900

    rsa: Fix decoding of PKCS#1 v1.5 and OAEP padding.
    * src/Makefile.am (libgcrypt_la_SOURCES): Add const-time.h and
    const-time.c.
    * src/const-time.h (ct_not_equal_byte, sexp_null_cond): New.
    (ct_memequal): New from NetBSD, modified return type and name.
    * src/const-time.c: New.
    * cipher/rsa-common.c (_gcry_rsa_pkcs1_decode_for_enc): Examine whole
    sequence of the byte-array.  Use N0 to find the separator position, with
    ct_not_equal_byte.  Return the MPI even when the case of an error.
    * cipher/rsa-common.c (_gcry_rsa_oaep_decode): Use ct_memequal to
    check LHASH.  Examine all the sequence of the byte-array.  Use N1 to
    find the separator of 0x01.  Return the MPI even when the case of an
    error.
    * cipher/rsa.c (rsa_decrypt): Always build a SEXP.
    
    --
    
    Note: For architecture(s) which may result branch in comparison of
    byte, configure script should emit POSSIBLE_BRANCH_IN_BYTE_COMPARISON.
    
    Reported-by: Hubert Kario <hkario@redhat.com>
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

---
+++ src/const-time.c
@@ -0,0 +1,51 @@
+/* const-time.c  -  Constant-time functions
+ *      Copyright (C) 2023  g10 Code GmbH
+ *
+ * This file is part of Libgcrypt.
+ *
+ * Libgcrypt is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * Libgcrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "g10lib.h"
+
+/* Originally in NetBSD.
+ *
+ * Written by Matthias Drochner <drochner@NetBSD.org>.
+ * Public domain.
+ *
+ * Modified the function name and return type to unsigned.
+ */
+unsigned int
+ct_memequal (const void *b1, const void *b2, size_t len)
+{
+	const unsigned char *c1 = b1, *c2 = b2;
+	unsigned int res = 0;
+
+	while (len--)
+		res |= *c1++ ^ *c2++;
+
+	/*
+	 * Map 0 to 1 and [1, 256) to 0 using only constant-time
+	 * arithmetic.
+	 *
+	 * This is not simply `!res' because although many CPUs support
+	 * branchless conditional moves and many compilers will take
+	 * advantage of them, certain compilers generate branches on
+	 * certain CPUs for `!res'.
+	 */
+	return (1 & ((res - 1) >> 8));
+}
---
+++ src/const-time.h
@@ -0,0 +1,53 @@
+/* const-time.h  -  Constant-time functions
+ *      Copyright (C) 2023  g10 Code GmbH
+ *
+ * This file is part of Libgcrypt.
+ *
+ * Libgcrypt is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * Libgcrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Return 1 if it's not same, 0 if same.
+ */
+static inline unsigned int
+ct_not_equal_byte (unsigned char b0, unsigned char b1)
+{
+#ifdef POSSIBLE_BRANCH_IN_BYTE_COMPARISON
+  unsigned int diff;
+
+  diff = b0;
+  diff ^= b1;
+
+  return (0U - diff) >> (sizeof (unsigned int)*8 - 1);
+#else
+  return b0 != b1;
+#endif
+}
+
+/*
+ *  Return NULL when OP_ENABLED=1
+ *  otherwise, return W
+ */
+static inline gcry_sexp_t
+sexp_null_cond (gcry_sexp_t w, unsigned long op_enable)
+{
+  static volatile uintptr_t vone = 1;
+  size_t mask = (uintptr_t)op_enable - vone;
+
+  return (gcry_sexp_t)(void *)((uintptr_t)w & mask);
+}
+
+/* Compare byte-arrays of length LEN, return 1 if it's same, 0 otherwise.
+   We use pointer of void *, so that it can be used with any structure.  */
+unsigned int ct_memequal (const void *b1, const void *b2, size_t len);
