88abccb3dc47039ff17324e2fd02a45272a738e3
NIIBE Yutaka <gniibe@fsij.org>
2023-11-17 15:23:43 +0900

    Experiment if the API works for DHKEM(X25519, HKDF-SHA256).
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- cipher/kem.c
+++ cipher/kem.c
@@ -37,6 +37,79 @@ _kem_random (void *ctx, size_t length, uint8_t * dst)
   _gcry_randomize (dst, length, GCRY_STRONG_RANDOM);
 }
 
+
+/* The generator of Curve25519.  */
+static unsigned char curve25519_G[32] = { 0x09 };
+
+static gpg_err_code_t
+ecc_kem_decap (int algo, const void *seckey, const void *ciphertext,
+               void *shared_secret)
+{
+  gpg_err_code_t err;
+  int curveid;
+  unsigned char public[32];
+  unsigned char ecdh[32];
+  unsigned char *p;
+  unsigned char labeled_ikm[7+5+7+32];
+  unsigned char labeled_info[2+7+5+13+32+32];
+  gcry_kdf_hd_t hd;
+  unsigned long param[1] = { 32 };
+
+  if (algo != GCRY_KEM_DHKEM_X25519_HKDF_SHA256)
+    return GPG_ERR_UNKNOWN_ALGORITHM;
+
+  /* From here, it's only for the DHKEM(X25519, HKDF-SHA256).  */
+
+  curveid = GCRY_ECC_CURVE25519;
+
+  /* Compute the public key.  */
+  err = _gcry_ecc_mul_point (curveid, public, seckey, curve25519_G);
+  if (err)
+    return err;
+
+  /* Do ECDH.  */
+  err = _gcry_ecc_mul_point (curveid, ecdh, seckey, ciphertext);
+  if (err)
+    return err;
+
+  p = labeled_ikm;
+  memcpy (p, "HPKE-v1", 7);
+  p += 7;
+  memcpy (p, "KEM\x00\x20", 5); /* suite_id */
+  p += 5;
+  memcpy (p, "eae_prk", 7);
+  p += 7;
+  memcpy (p, ecdh, 32);
+
+  p = labeled_info;
+  memcpy (p, "\x00\x20", 2);    /* length */
+  p += 2;
+  memcpy (p, "HPKE-v1", 7);     /* suite_id */
+  p += 7;
+  memcpy (p, "KEM\x00\x20", 5); /* suite_id */
+  p += 5;
+  memcpy (p, "shared_secret", 13);
+  p += 13;
+  /* kem_context */
+  memcpy (p, ciphertext, 32);
+  p += 32;
+  memcpy (p, public, 32);
+  p += 32;
+
+  err = _gcry_kdf_open (&hd, GCRY_KDF_HKDF, GCRY_MAC_HMAC_SHA256, param, 1,
+                        labeled_ikm, sizeof (labeled_ikm),
+                        NULL, 0, NULL, 0, labeled_info, sizeof (labeled_info));
+  if (err)
+    return err;
+
+  err = _gcry_kdf_compute (hd, NULL);
+  if (!err)
+    err = _gcry_kdf_final (hd, 32, shared_secret);
+  _gcry_kdf_close (hd);
+
+  return err;
+}
+
 gcry_err_code_t
 _gcry_kem_keypair (int algo, void *pubkey, void *seckey)
 {
@@ -50,6 +123,8 @@ _gcry_kem_keypair (int algo, void *pubkey, void *seckey)
     case GCRY_KEM_MLKEM1024:
       mlkem_keypair (algo, pubkey, seckey);
       return GPG_ERR_NO_ERROR;
+    case GCRY_KEM_DHKEM_X25519_HKDF_SHA256:
+      return GPG_ERR_NOT_IMPLEMENTED; /* Not yet.  */
     default:
       return GPG_ERR_UNKNOWN_ALGORITHM;
     }
@@ -70,6 +145,8 @@ _gcry_kem_encap (int algo,
     case GCRY_KEM_MLKEM768:
     case GCRY_KEM_MLKEM1024:
       return mlkem_encap (algo, ciphertext, shared_secret, pubkey);
+    case GCRY_KEM_DHKEM_X25519_HKDF_SHA256:
+      return GPG_ERR_NOT_IMPLEMENTED; /* Not yet.  */
     default:
       return GPG_ERR_UNKNOWN_ALGORITHM;
     }
@@ -90,6 +167,8 @@ _gcry_kem_decap (int algo,
     case GCRY_KEM_MLKEM768:
     case GCRY_KEM_MLKEM1024:
       return mlkem_decap (algo, shared_secret, ciphertext, seckey);
+    case GCRY_KEM_DHKEM_X25519_HKDF_SHA256:
+      return ecc_kem_decap (algo, seckey, ciphertext, shared_secret);
     default:
       return GPG_ERR_UNKNOWN_ALGORITHM;
     }
