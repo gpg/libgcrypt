d698ed5386e8e3be43296e77938f513261523503
NIIBE Yutaka <gniibe@fsij.org>
2025-03-27 11:12:33 +0900

    mpi:ec: Keep A untouched in ec_get_a_is_pminus3.
    * mpi/ec.c (ec_get_a_is_pminus3): Change the calculation
    so that it keeps the MPI of A untouched.
    
    --
    
    Note that mpi_cmp with A may normalize the MPI.
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- mpi/ec.c
+++ mpi/ec.c
@@ -728,8 +728,8 @@ ec_get_a_is_pminus3 (mpi_ec_t ec)
     {
       ec->t.valid.a_is_pminus3 = 1;
       tmp = mpi_alloc_like (ec->p);
-      mpi_sub_ui (tmp, ec->p, 3);
-      ec->t.a_is_pminus3 = !mpi_cmp (ec->a, tmp);
+      mpi_sub (tmp, ec->p, ec->a);
+      ec->t.a_is_pminus3 = !mpi_cmp_ui (tmp, 3);
       mpi_free (tmp);
     }
 
