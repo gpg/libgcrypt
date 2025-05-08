b052a0f3c751ebb793b1b3ed6756f78d9cb9a82a
NIIBE Yutaka <gniibe@fsij.org>
2025-05-08 11:27:03 +0900

    Let us start integrating Dilithium into libgcrypt.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

---
+++ cipher/dilithium.h
@@ -0,0 +1,52 @@
+/* dilithium.h - the Dilithium (header)
+ * Copyright (C) 2025 g10 Code GmbH
+ *
+ * This file was modified for use by Libgcrypt.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <https://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * You can also use this file under the same licence of original code.
+ * SPDX-License-Identifier: CC0 OR Apache-2.0
+ *
+ */
+/*
+  Original code from:
+
+  Repository: https://github.com/pq-crystals/dilithium.git
+  Branch: master
+  Commit: 444cdcc84eb36b66fe27b3a2529ee48f6d8150c2
+
+  Licence:
+  Public Domain (https://creativecommons.org/share-your-work/public-domain/cc0/);
+  or Apache 2.0 License (https://www.apache.org/licenses/LICENSE-2.0.html).
+
+  Authors:
+        Léo Ducas
+        Eike Kiltz
+        Tancrède Lepoint
+        Vadim Lyubashevsky
+        Gregor Seiler
+        Peter Schwabe
+        Damien Stehlé
+
+  Dilithium Home: https://github.com/pq-crystals/dilithium.git
+ */
+/* Standalone use is possible either with DILITHIUM_MODE defined with
+ * the value (2, 3, or 5), or not defined.  For the latter, routines
+ * for three variants are available.
+ */
+#ifndef DILITHIUM_H
+#define DILITHIUM_H
+#endif
