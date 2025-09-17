9e1ee6efea9d1b09653fd41e41509d9c8593196b
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2025-09-17 19:14:02 +0300

    mceliece6688128f: harden mask generation against branch optimization
    * cipher/mceliece6688128f.c (crypto_int64_negative_mask)
    (crypto_int16_negative_mask, crypto_int32_negative_mask)
    (crypto_uint64_signed_negative_mask, crypto_uint16_signed_negative_mask)
    (crypto_uint32_signed_negative_mask, mask_nonzero, mask_leq, vec_cmov)
    (layer, gen_e, gf_mul, same_mask, pk_gen): Generate masks with
    const-time helper function.
    * src/const-time.h (ct_u64_gen_mask, ct_u64_gen_inv_mask): New.
    --
    
    Cherry-pick master commit of:
    	4012e9a037c5bf9f296ca89a6e94bb3600ec0d35
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- src/const-time.h
+++ src/const-time.h
@@ -114,6 +114,7 @@ unsigned int _gcry_ct_memequal (const void *b1, const void *b2, size_t len);
 DEFINE_CT_TYPE_GEN_MASK(uintptr, uintptr_t)
 DEFINE_CT_TYPE_GEN_MASK(ulong, unsigned long)
 DEFINE_CT_TYPE_GEN_MASK(int16, int16_t)
+DEFINE_CT_TYPE_GEN_MASK(u64, u64)
 
 /*
  * Return all bits set if A is 0 and return 1 otherwise.
@@ -139,6 +140,7 @@ DEFINE_CT_TYPE_GEN_MASK(int16, int16_t)
 DEFINE_CT_TYPE_GEN_INV_MASK(uintptr, uintptr_t)
 DEFINE_CT_TYPE_GEN_INV_MASK(ulong, unsigned long)
 DEFINE_CT_TYPE_GEN_INV_MASK(int16, int16_t)
+DEFINE_CT_TYPE_GEN_INV_MASK(u64, u64)
 
 /*
  *  Return A when OP_ENABLED=1
@@ -155,6 +157,7 @@ DEFINE_CT_TYPE_GEN_INV_MASK(int16, int16_t)
 DEFINE_CT_TYPE_SELECT_FUNC(uintptr, uintptr_t)
 DEFINE_CT_TYPE_SELECT_FUNC(ulong, unsigned long)
 DEFINE_CT_TYPE_SELECT_FUNC(int16, int16_t)
+DEFINE_CT_TYPE_SELECT_FUNC(u64, u64)
 
 /*
  *  Return NULL when OP_ENABLED=1
